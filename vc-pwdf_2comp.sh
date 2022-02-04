f7e4dca23ce6c0ce54c311bfd671c41613cc992e
R. Alex Mayo <ra.mayo@dal.ca>
2022-02-04 17:32:50 -0400

    fixed issue with wrong angles used for axes-swapped cell in 2comp.sh

--- vc-pwdf_2comp.sh
+++ vc-pwdf_2comp.sh
@@ -32,45 +32,45 @@ best to check both structures are critic2 readable before starting.
 
 
 ### user input of 2 structures to compare
-read -p "Structure 1 [.cif]: " ref1
-ref=${ref1%.cif}
-read -p "Structure 2 [.cif]: " file2
-file=${file2%.cif}
-echo "$ref1  $ref" 
-echo "$file2 $file"
+read -p "Structure 1 [.cif]: " xtal11
+xtal1=${xtal11%.cif}
+read -p "Structure 2 [.cif]: " xtal22
+xtal2=${xtal22%.cif}
+echo "$xtal11  $xtal1" 
+echo "$xtal22 $xtal2"
 
 read -p "Would you like to convert the structures to their Niggli reduced cell (recommended)? [y/n] " n_red
 if [ ${n_red,,} = y ]
 then
 ### use critic2 to process and normalize the formatting of the structure files
-	for ciff in $ref1 $file2
+	for ciff in $xtal11 $xtal22
 	do
 		sed -i "s/\r//g" $ciff
 	done
 	cat > cif_processing.cri << EOF
-crystal $ref1
+crystal $xtal11
 newcell niggli
-write ${ref}_c.cif
-write ${ref}.res
-crystal $file2
+write ${xtal1}_c.cif
+write ${xtal1}.res
+crystal $xtal22
 newcell niggli
-write ${file}_c.cif
-write ${file}.res
+write ${xtal2}_c.cif
+write ${xtal2}.res
 EOF
 
 elif [ ${n_red,,} = n ]
 then
-	for ciff in $ref1 $file2
+	for ciff in $xtal11 $xtal22
 	do
 		sed -i "s/\r//g" $ciff
 	done
 	cat > cif_processing.cri << EOF
-crystal $ref1
-write ${ref}_c.cif
-write ${ref}.res
-crystal $file2
-write ${file}_c.cif
-write ${file}.res
+crystal $xtal11
+write ${xtal1}_c.cif
+write ${xtal1}.res
+crystal $xtal22
+write ${xtal2}_c.cif
+write ${xtal2}.res
 EOF
 fi
 
@@ -78,8 +78,8 @@ critic2 < cif_processing.cri > cif_processing.cro
 rm cif_processing.*
 
 ### check to make sure both strucutres are same space group, give option to continue or quit if not same
-spgp_1=$(grep "_space_group_IT_number" ${ref}_c.cif | awk -F ' ' '{print$2}')
-spgp_2=$(grep "_space_group_IT_number" ${file}_c.cif | awk -F ' ' '{print$2}')
+spgp_1=$(grep "_space_group_IT_number" ${xtal1}_c.cif | awk -F ' ' '{print$2}')
+spgp_2=$(grep "_space_group_IT_number" ${xtal2}_c.cif | awk -F ' ' '{print$2}')
 echo "Space_groups: $spgp_1  $spgp_2"
 if [ $spgp_1 -ne $spgp_2 ]
 then
@@ -97,60 +97,60 @@ fi
 
 
 ### check volumes to determine which structure to use as reference and which to modify
-read -p "Do you want to hold one structure constant to compare against?[y/n]
-" hold_choice
-if [ ${hold_choice,,} = y ]
+read -p "Do you want to pick which structure is used as the reference (default is larger volume structure)?[y/n]
+" ref_choice
+if [ ${ref_choice,,} = y ]
 then
-	read -p "Which structure would you like to hold constant? [$ref OR $file]
-	" hold_cell
-	if [ $hold_cell == $ref ]
+	read -p "Which structure would you like to hold constant? [$xtal1 OR $xtal2]
+	" ref_cell
+	if [ $ref_cell == $xtal1 ]
 	then
-		point_o=$ref
-		original=$file
-	elif [ $hold_cell == $file ] 
+		ref=$xtal1
+		comp=$xtal2
+	elif [ $ref_cell == $xtal2 ] 
 	then
-		original=$ref
-		point_o=$file
+		comp=$xtal1
+		ref=$xtal2
 	fi
-elif [ ${hold_choice,,} = n ]
+elif [ ${ref_choice,,} = n ]
 then
-	echo "Structure with largest unit cell will be used as reference cell for comparison"
-	V_1=$(grep "_cell_volume" ${ref}_c.cif | awk -F ' ' '{print$2}')
-	V_2=$(grep "_cell_volume" ${file}_c.cif | awk -F ' ' '{print$2}')
+	echo "Structure with largest unit cell volume  will be used as reference for comparison"
+	V_1=$(grep "_cell_volume" ${xtal1}_c.cif | awk -F ' ' '{print$2}')
+	V_2=$(grep "_cell_volume" ${xtal2}_c.cif | awk -F ' ' '{print$2}')
 	if (( $(bc -l <<< "$V_1 < $V_2") ))
 	then
-		point_o=$file
-		original=$ref
+		ref=$xtal2
+		comp=$xtal1
 	else
-		original=$file
-		point_o=$ref
+		comp=$xtal2
+		ref=$xtal1
 	fi
 fi
-echo "Reference structure: $point_o     Structure being corrected for comparison: $original"
+echo "Reference structure: $ref     Structure being corrected for comparison: $comp"
 
 
-a_pc=$(grep CELL ${point_o}.res | awk -F ' ' '{print$3}')
-b_pc=$(grep CELL ${point_o}.res | awk -F ' ' '{print$4}')
-c_pc=$(grep CELL ${point_o}.res | awk -F ' ' '{print$5}')
-alpha_pc=$(grep CELL ${point_o}.res | awk -F ' ' '{print$6}')
-beta_pc=$(grep CELL ${point_o}.res | awk -F ' ' '{print$7}')
-gamma_pc=$(grep CELL ${point_o}.res | awk -F ' ' '{print$8}')
+a_pc=$(grep CELL ${ref}.res | awk -F ' ' '{print$3}')
+b_pc=$(grep CELL ${ref}.res | awk -F ' ' '{print$4}')
+c_pc=$(grep CELL ${ref}.res | awk -F ' ' '{print$5}')
+alpha_pc=$(grep CELL ${ref}.res | awk -F ' ' '{print$6}')
+beta_pc=$(grep CELL ${ref}.res | awk -F ' ' '{print$7}')
+gamma_pc=$(grep CELL ${ref}.res | awk -F ' ' '{print$8}')
 a_b_pc=$(bc -l <<< "$a_pc - $b_pc")
 a_c_pc=$(bc -l <<< "$a_pc - $c_pc")
 b_c_pc=$(bc -l <<< "$b_pc - $c_pc")
-echo "$point_o axes length differences: $a_b_pc $a_c_pc $b_c_pc"
+echo "$ref axes length differences: $a_b_pc $a_c_pc $b_c_pc"
 
-a_oc=$(grep CELL ${original}.res | awk -F ' ' '{print$3}')
-b_oc=$(grep CELL ${original}.res | awk -F ' ' '{print$4}')
-c_oc=$(grep CELL ${original}.res | awk -F ' ' '{print$5}')
-alpha_oc=$(grep CELL ${original}.res | awk -F ' ' '{print$6}')
-beta_oc=$(grep CELL ${original}.res | awk -F ' ' '{print$7}')
-gamma_oc=$(grep CELL ${original}.res | awk -F ' ' '{print$8}')
+a_oc=$(grep CELL ${comp}.res | awk -F ' ' '{print$3}')
+b_oc=$(grep CELL ${comp}.res | awk -F ' ' '{print$4}')
+c_oc=$(grep CELL ${comp}.res | awk -F ' ' '{print$5}')
+alpha_oc=$(grep CELL ${comp}.res | awk -F ' ' '{print$6}')
+beta_oc=$(grep CELL ${comp}.res | awk -F ' ' '{print$7}')
+gamma_oc=$(grep CELL ${comp}.res | awk -F ' ' '{print$8}')
 a_b_oc=$(bc -l <<< "$a_oc - $b_oc")
 a_c_oc=$(bc -l <<< "$a_oc - $c_oc")
 b_c_oc=$(bc -l <<< "$b_oc - $c_oc")
-spgp_oc=$(grep "_space_group_IT_number" ${original}_c.cif | awk -F ' ' '{print$2}')
-echo "$original axes length differences: $a_b_oc $a_c_oc $b_c_oc"
+spgp_oc=$(grep "_space_group_IT_number" ${comp}_c.cif | awk -F ' ' '{print$2}')
+echo "$comp axes length differences: $a_b_oc $a_c_oc $b_c_oc"
 
 
 # axis lengths first
@@ -159,55 +159,57 @@ echo "$original axes length differences: $a_b_oc $a_c_oc $b_c_oc"
 axdiff=1.0 # work to fix this to 5% instead of an absolute value in Angstroms
 if (( $(bc -l <<< "$a_b_pc <= $axdiff && $a_b_pc >= -$axdiff") )) || (( $(bc -l <<< "$a_c_pc <= $axdiff && $a_c_pc >= -$axdiff") )) || (( $(bc -l <<< "$b_c_pc <= $axdiff && $b_c_pc >= -$axdiff") ))
 then
-	echo "WARNING, at least one of the axes of $point_o is within $axdiff A of another axis of the unit cell"
+	echo "WARNING, at least one of the axes of $ref is within $axdiff A of another axis of the unit cell"
 fi
+
 if (( $(bc -l <<< "$a_b_oc <= $axdiff && $a_b_oc >= -$axdiff" ) )) || (( $(bc -l <<< "$a_c_oc <= $axdiff && $a_c_oc >= -$axdiff" ) )) || (( $(bc -l <<< "$b_c_oc <= $axdiff && $b_c_oc >= -$axdiff" ) ))
 then
-	echo "WARNING, at least one of the axes of $original is within $axdiff A of another axis of the unit cell"
+	echo "WARNING, at least one of the axes of $comp is within $axdiff A of another axis of the unit cell"
 	if (( $(bc -l <<< "$a_b_oc <= $axdiff && $a_b_oc >= -$axdiff") )) 
 	then
-		echo "crystal ${original}.res
+		echo "crystal ${comp}.res
 		NEWCELL 0 1 0 1 0 0 0 0 1
-		write ${original}_ab.res" | critic2 -q
+		write ${comp}_ab.res" | critic2 -q
 		ax_swpd="_ab"
 	fi
 	if (( $(bc -l <<< "$a_c_oc <= $axdiff && $a_c_oc >= -$axdiff") )) 
 	then
-		echo "crystal ${original}.res
+		echo "crystal ${comp}.res
 		NEWCELL 0 0 1 0 1 0 1 0 0
-		write ${original}_ac.res" | critic2 -q
+		write ${comp}_ac.res" | critic2 -q
 		ax_swpd="_ac"
 	fi
 	if (( $(bc -l <<< "$b_c_oc <= $axdiff && $b_c_oc >= -$axdiff") ))
 	then
-		echo "crystal ${original}.res
+		echo "crystal ${comp}.res
 		NEWCELL 1 0 0 0 0 1 0 1 0
-		write ${original}_bc.res" | critic2 -q
+		write ${comp}_bc.res" | critic2 -q
 		ax_swpd="_bc"
 	fi
-	echo "possible swapped UC axes structure ${original}${ax_swpd}.res"
-	check_structure="${original}${ax_swpd}.res"
+	echo "possible swapped UC axes structure ${comp}${ax_swpd}.res"
+	check_structure="${comp}${ax_swpd}.res"
 fi
 
 # Angles second
 if (( $(bc -l <<< "$alpha_pc == 90 && $alpha_oc != 90" ) )) || (( $(bc -l <<< "$beta_pc == 90 && $beta_oc != 90" ) )) || (( $(bc -l <<< "$gamma_pc == 90 && $gamma_oc != 90" ) ))
 then
-	echo "ERROR, one of the cell angles of $original does not match up with the reference structure (90 vs !=90) $original will not undergo volume correction"
+	echo "ERROR, one of the cell angles of $comp does not match up with the reference structure (90 vs !=90) $comp will not undergo volume correction"
+
 else
 	echo "$alpha_n  $beta_n  $gamma_n "
-	grep 'CELL' ${point_o}.res > swap_cell.tmp
+	grep 'CELL' ${ref}.res > swap_cell.tmp
 	### Make UC parameter correction
-	point_ocell=`grep CELL ${point_o}.res`
+	refcell=`grep CELL ${ref}.res`
 	point_cell=`grep CELL swap_cell.tmp`
 	echo "the unit cell used to standardise is:
-	$point_ocell"
-	og_cell=`grep CELL ${original}.res`
-	echo "exchanging unit cell of ${original}.res:
+	$refcell"
+	og_cell=`grep CELL ${comp}.res`
+	echo "exchanging unit cell of ${comp}.res:
 	$og_cell
 	with:
 	$point_cell"
-	sed "s/$og_cell/$point_cell/" ${original}.res > ${original}_VC.res
-	echo "${original}_VC.res " > compare.lst 
+	sed "s/$og_cell/$point_cell/" ${comp}.res > ${comp}_VC.res
+	echo "${comp}_VC.res " >> compare.lst 
 fi
 
 ####### duplicate correction for UC-axes swaps (if any)
@@ -217,17 +219,17 @@ then
 	beta_xc=$(grep CELL $check_structure | awk -F ' ' '{print$7}')
 	gamma_xc=$(grep CELL $check_structure | awk -F ' ' '{print$8}')
 	
-       	if (( $(bc -l <<< "$alpha_pc == 90 && $alpha_oc != 90" ) )) || (( $(bc -l <<< "$beta_pc == 90 && $beta_oc != 90" ) )) || (( $(bc -l <<< "$gamma_pc == 90 && $gamma_oc != 90" ) ))
+       	if (( $(bc -l <<< "$alpha_pc == 90 && $alpha_xc != 90" ) )) || (( $(bc -l <<< "$beta_pc == 90 && $beta_xc != 90" ) )) || (( $(bc -l <<< "$gamma_pc == 90 && $gamma_xc != 90" ) ))
         then
-                echo "ERROR, one of the cell angles of $original does not match up with the reference structure (90 vs !=90) $original will not undergo volume correction"
+                echo "ERROR, one of the cell angles of $check_structure does not match up with the reference structure (90 vs !=90) $check_structure will not undergo volume correction"
 	else
 		echo "$alpha_n  $beta_n  $gamma_n "
-		grep 'CELL' ${point_o}.res > swap_cell.tmp
+		grep 'CELL' ${ref}.res > swap_cell.tmp
 		### Make UC parameter correction
-		point_ocell=`grep CELL ${point_o}.res`
+		refcell=`grep CELL ${ref}.res`
 		point_cell=`grep CELL swap_cell.tmp`
 		echo "the unit cell used to standardise is:
-		$point_ocell"
+		$refcell"
 		og_cell=`grep CELL $check_structure`
 		echo "exchanging unit cell of $check_structure :
 		$og_cell
@@ -237,6 +239,8 @@ then
 		echo "${check_structure%.res}_VC.res " >> compare.lst
 	fi
 fi
+
+
 
 ########## perform various transformations to account for lattice vectors used in Niggli cell
 
@@ -258,10 +262,10 @@ then
 	for tm in "${acute_tms[@]}"
 	do
 		echo $tm
-		echo "crystal ${original}.res
+		echo "crystal ${comp}.res
 		NEWCELL $tm
-		write ${original}_tm${count}.res" | critic2 -q
-		transform="${original}_tm${count}.res"
+		write ${comp}_tm${count}.res" | critic2 -q
+		transform="${comp}_tm${count}.res"
 		cp swap_cell.tmp swap_cell_${transform%.res}.tmp
 		alpha_tm=$(grep CELL $transform | awk -F ' ' '{print$6}')
 		beta_tm=$(grep CELL $transform | awk -F ' ' '{print$7}')
@@ -271,7 +275,7 @@ then
 		then
 		        echo "ERROR, one of the cell angles of $transform does not match up with the reference structure (90 vs !=90) $transform will not undergo volume correction"
 		else
-		        cat "swap_cell_${original_o%.cif}.tmp"
+		        cat "swap_cell_${comp_o%.cif}.tmp"
 		        swp_cell=`grep 'CELL' swap_cell_${transform%.res}.tmp`
 		        cp $transform ${transform%.res}_VC.res
 		        og_cell=`grep CELL ${transform%.res}_VC.res`
@@ -295,10 +299,10 @@ then
 	for tm in "${obtuse_short_tms[@]}"
 	do
 		echo $tm
-		echo "crystal ${original}.res
+		echo "crystal ${comp}.res
 		NEWCELL $tm
-		write ${original}_tm${count}.res" | critic2 -q
-		transform="${original}_tm${count}.res"
+		write ${comp}_tm${count}.res" | critic2 -q
+		transform="${comp}_tm${count}.res"
 		cp swap_cell.tmp swap_cell_${transform%.res}.tmp
 		alpha_tm=$(grep CELL $transform | awk -F ' ' '{print$6}')
 		beta_tm=$(grep CELL $transform | awk -F ' ' '{print$7}')
@@ -308,7 +312,7 @@ then
 		then
 		        echo "ERROR, one of the cell angles of $transform does not match up with the reference structure (90 vs !=90) $transform will not undergo volume correction"
 		else
-		        cat "swap_cell_${original_o%.cif}.tmp"
+		        cat "swap_cell_${comp_o%.cif}.tmp"
 		        swp_cell=`grep 'CELL' swap_cell_${transform%.res}.tmp`
 		        cp $transform ${transform%.res}_VC.res
 		        og_cell=`grep CELL ${transform%.res}_VC.res`
@@ -330,10 +334,10 @@ else
 	for tm in "${obtuse_tms[@]}"
 	do
 		echo $tm
-		echo "crystal ${original}.res
+		echo "crystal ${comp}.res
 		NEWCELL $tm
-		write ${original}_tm${count}.res" | critic2 -q
-		transform="${original}_tm${count}.res"
+		write ${comp}_tm${count}.res" | critic2 -q
+		transform="${comp}_tm${count}.res"
 		cp swap_cell.tmp swap_cell_${transform%.res}.tmp
 		alpha_tm=$(grep CELL $transform | awk -F ' ' '{print$6}')
 		beta_tm=$(grep CELL $transform | awk -F ' ' '{print$7}')
@@ -343,7 +347,7 @@ else
 		then
 		        echo "ERROR, one of the cell angles of $transform does not match up with the reference structure (90 vs !=90) $transform will not undergo volume correction"
 		else
-		        cat "swap_cell_${original_o%.cif}.tmp"
+		        cat "swap_cell_${comp_o%.cif}.tmp"
 		        swp_cell=`grep 'CELL' swap_cell_${transform%.res}.tmp`
 		        cp $transform ${transform%.res}_VC.res
 		        og_cell=`grep CELL ${transform%.res}_VC.res`
@@ -369,14 +373,14 @@ echo "
 
 running POWDIFF comparisons ...
 "
-echo "compare $point_o.cif $original.cif" | critic2 -q > powdiff.cro
+echo "compare $ref.cif $comp.cif" | critic2 -q > powdiff.cro
 # 1 - make a file with matching structures
-echo -n "$original.cif    " >> result_out.lst
+echo -n "$comp.cif    " >> result_out.lst
 grep DIFF powdiff.cro | awk '{print$4}' | tail -1 >> result_out.lst
 
 for comparison in `cat compare.lst`
 do
-	echo "compare $point_o.cif $comparison" | critic2 -q > powdiff.cro
+	echo "compare $ref.cif $comparison" | critic2 -q > powdiff.cro
         # 1 - make a file with matching structures
         echo -n "$comparison    " >> result_out.lst
         grep DIFF powdiff.cro | awk '{print$4}' | tail -1 >> result_out.lst
@@ -388,12 +392,12 @@ echo "structure POWDIFF  a       b       c       alpha   beta    gamma" > result
 for i in `awk '{print$1}' ordered_powdiff.txt`
 do
 	pow=$(grep "$i" ordered_powdiff.txt | awk '{print$2}')
-	a=$(grep CELL $i | awk -F ' ' '{print$3}')
-        b=$(grep CELL $i | awk -F ' ' '{print$4}')
-        c=$(grep CELL $i | awk -F ' ' '{print$5}')
-        alpha=$(grep CELL $i | awk -F ' ' '{print$6}')
-        beta=$(grep CELL $i | awk -F ' ' '{print$7}')
-        gamma=$(grep CELL $i | awk -F ' ' '{print$8}')
+	a=$(grep CELL ${i%%.*}.res | awk -F ' ' '{print$3}')
+        b=$(grep CELL ${i%%.*}.res | awk -F ' ' '{print$4}')
+        c=$(grep CELL ${i%%.*}.res | awk -F ' ' '{print$5}')
+        alpha=$(grep CELL ${i%%.*}.res | awk -F ' ' '{print$6}')
+        beta=$(grep CELL ${i%%.*}.res | awk -F ' ' '{print$7}')
+        gamma=$(grep CELL ${i%%.*}.res | awk -F ' ' '{print$8}')
 	echo "${i%.res} $pow $a $b $c $alpha $beta $gamma" >> results_table.rslt
 done
 
@@ -402,4 +406,5 @@ cat ordered_powdiff.txt
 ### clean-up
 rm *.cro
 rm *.tmp
+
 
